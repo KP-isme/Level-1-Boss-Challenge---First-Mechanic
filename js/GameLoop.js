3157bce5c109cb74cdc4df9dee61d61fb7e388da
KP-isme <kpnot2day@gmail.com>
2026-05-21 13:33:52 -0400

    The last one wasn't the right files also updated it so that it does the right thing for the right sscore and adding stuff like text so now it works like it should be working, yep yep
    yep yep

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -1,123 +1,192 @@
+var canvas;
+var context;
+var player;
+var timer;
+var interval = 1000/60; //this means the animation is going to play in 60 milliseconds
 
 
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-
-var interval = 1000/60;
-var timer = setInterval(animate, interval);
-
-player = new GameObject;
-
-player.force = 5;
-
-follower = new GameObject
-
-follower.force = 1;
+var frictionX = 1;
+var frictionY = 1;
+var gravity = 0;
 
 
-follower.x = 200;
-follower.y = 200;
-follower.color =  "#0000ff";
+var score = 0;
+
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
+
+// player = new GameObject(0,0,100,100, "#00ff00");
+player = new GameObject(50, canvas.height/2, 100, 100, "#ffff00");
+player.vx = 10;
+player.vy = 10;
 
 
-var fX = 0.5;
-var fY = 0.5;
 
+
+
+/*
+npc1 = new GameObject(300, canvas.height/2, 100, 100, "#00ff00");
+npc2 = new GameObject(600, canvas.height/2, 100, 100, "#0000ff");
+npc3 = new GameObject(900, canvas.height/2, 100, 100, "#ff00ff");
+*/
+
+
+timer = setInterval(animate, interval);
 
 function animate()
 {
-    context.clearRect(0,0,canvas.clientWidth,canvas.height);
+    
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    angularMovement();
-
-    player.drawTriangle();
-    follower.drawTriangle();
-
-}
-
-
-
-function angularMovement()
-{
-    if(w)
+    context.fillStyle = "black";
+context.font = "30px Arial";
+context.fillText("Score: " + score, 100, 190);
+    
+/*
+    if (d)
     {
-        var radians = player.angle * Math.PI/180;
-        player.ax = Math.cos(radians);
-        player.ay = Math.sin(radians);
-
-        player.vx += player.ax * player.force;
-        player.vy += player.ay * player.force;
-        
-
+        player.x += 4
     }
 
-
-    if(s)
+    if (a)
     {
-        var radian = player.angle * Math.PI/180;
-
-
-
+        player.x -= 4
     }
-    if(a)
-    {
-        player.angle -= 10;
-    }
-    if(d)
-    {
-        player.angle += 10;
-    }
+  */  
+
+    doHandleAcceleration();
+    doHandleFriction();
+    doHandleGravity();
+    doHandlePosition();
+    doCheckBottomBounds();
 
 
-    player.vx *= fX;
-    player.vy *= fX;
 
     player.move();
+    if (player.x > canvas.width + player.width/2)
+    {
+        player.x = player-width/2
+    }
 
-    point();
+/*//npc collition
+    if (npc1.collisionCheck(player))
+    {
+        npc1.color = "yellow";
+        npc1.width = 125;
+    }
+    else
+    {
+        npc1.color = "green";
+        npc1.width = 100;
+    }
+
     
-    follow();
 
+    if (npc2.collisionCheck(player))
+    {
+        context.strokeRect(npc2.x-npc2.width/2, npc2.y-npc2.height/2, npc2.width, npc2.height);
+    }
+
+    if (npc3.collisionCheck(player))
+    {
+        player.x = player.preX
+    }
+    else 
+    {
+        player.preX = player.x;
+    }
+*/ 
+
+    // all the things under would have made it bounce off walls and move (if you change the player.vx value)
+
+    if (player.x > canvas.width - player.width/2) 
+    {
+        //player.x = +player.width/2;  //whats this do? This make sure that all of the player is spawned behind the left side.
+        player.vx = -5;
+        player.vy = -10;
+        score++;
+    }
+    if (player.x < 0 + player.width/2) 
+    {
+        //player.x = +player.width/2;  //whats this do? This make sure that all of the player is spawned behind the left side.
+        player.vx = 10;
+        score++;
+    }
+    if (player.y > 700 + player.height/2) 
+    {
+        //player.x = +player.width/2;  //whats this do? This make sure that all of the player is spawned behind the left side.
+        player.vy = -10;
+        player.vx = -15;
+        score++;
+    }
+    if (player.y < 0 + player.height/2) 
+    {
+        //player.x = +player.width/2;  //whats this do? This make sure that all of the player is spawned behind the left side.
+        player.vy = +10;
+        player.vx = +5;
+        score++;
+    }
+    player.drawCircle();
+    //extra dudes
+    // npc1.drawCircle();
+    // npc2.drawCircle();
+    // npc3.drawRect();
 }
 
- function point()
- {
-
-    var dx = player.x - follower.x;
-    var dy = player.y - follower.y;
-
-    var radians = Math.atan2(dy,dx);
-
-    follower.angle = radians * 180/Math.PI;
 
 
- }
-
-
-function follow()
+//new funciton for the functions we called (doHandleAcceleration)
+function doHandleAcceleration ()
 {
-    var dx = player.x - follower.x;
-    var dy = player.y - follower.y;
+    if (d) 
+        {
+            player.vx += player.ax * player.force;
+        }
+    if (a) 
+        {
+            player.vx += player.ax * -player.force;
+        }
 
-    var radians = Math.atan2(dy,dx);
-    follower.angle = radians * 180/Math.PI;
-
-    follower.vx = Math.cos(radians)*follower.force;
-    follower.vy = Math.sin(radians)*follower.force;
-
-    follower.x += follower.vx * 2; 
-    follower.y += follower.vy * 2;
- 
 }
 
 
-
-/* 
-//for loop for the array
-
-for(var i = 0; i < theAvengers.length; i++)
+//the friction is math out by multiplying the player's current speed to the value of frition (friction needs to be lower than 1 and higher than 0)
+function doHandleFriction () 
 {
-    console.log(theAvengers[i]);
+
+    player.vx *= frictionX;
+
 }
 
-*/
+function doHandleGravity () 
+{
+    player.vy += gravity;
+}
+
+function doHandlePosition () 
+{
+    player.x += player.vx;
+    player.y += player.vy;
+}
+
+
+function doCheckBottomBounds ()
+{
+    if (player.y > canvas.height - player.height/2)
+    {
+        player.y = canvas.height - player.height/2;
+        player.vy = 0;
+        doJump();
+    }
+}
+function doJump()
+{
+    if (w)
+    {
+        player.vy = -20;
+    }
+    if (release(w))
+    {
+        player.vy = 20;
+    }
+}
--- js/Utilities/math.js
+++
@@ -1,8 +0,0 @@
-//math funcitons
-
-
-function rand(low, high)
-{
-    return Math.random() * (high - low) + low; 
-}
- 
